b7f00a3c0b735cd774b52c6b882827c7229dc0b9
malepsyche <114489469+malepsyche@users.noreply.github.com>
2024-03-18 14:38:12 +0800

    Added strava auth button

--- frontend/src/app/profile/features/profile.page.ts
+++ frontend/src/app/profile/features/profile.page.ts
@@ -14,4 +14,10 @@ import { PageWrapperComponent } from '@app/shared/ui/page-wrapper/page-wrapper.c
 
 export class ProfilePage {
   content = [{name:'June 10 Hike'},{name:'June 20 Hike'},{name:'June 30 Hike'},]
+  
+  openStravaAuthLink() {
+    let clientId: string = "121252"
+    let stravaUrl: string = `https://www.strava.com/oauth/authorize?client_id=${clientId}&response_type=code&redirect_uri=http://localhost/exchange_token&approval_prompt=force&scope=read`
+    window.open(stravaUrl)
+  }
 }
