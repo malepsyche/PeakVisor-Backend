373bfc1377ae36a6e7fc5204a533b71e217f254e
Your Name <you@example.com>
2024-03-18 11:50:32 +0000

    Changed env backendUrl

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    backendUrl: 'http://localhost:3000',
-  }
+    // backendUrl: 'http://localhost:3000',
+    backendUrl: 'http://peakvisor.malepsyche.com/api'
+};
