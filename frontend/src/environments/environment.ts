3198bc2cadc30a455bab3189f20e81f257ff94ee
Your Name <you@example.com>
2024-03-18 12:22:39 +0000

    idk

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    backendUrl: 'http://localhost:3000', 
-    // backendUrl: 'http://peakvisor.malepsyche.com/api'
+    // backendUrl: 'http://localhost:3000', 
+    backendUrl: 'http://peakvisor.malepsyche.com/api'
 };
