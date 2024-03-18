bfe5a69a9add033a9d6a6ac05af063b090658951
malepsyche <114489469+malepsyche@users.noreply.github.com>
2024-03-18 14:37:51 +0800

    Added stravaAPI Oauth flows in routes, controller and services dir

---
+++ backend/src/services/strava/stravaService.ts
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+class StravaService {
+    private clientId: string = process.env.STRAVA_CLIENT_ID || '';
+    private clientSecret: string = process.env.STRAVA_CLIENT_SECRET || '';
+
+    getStravaAuthUrl(redirectUri: string): string {
+        return `http://www.strava.com/oauth/authorize?client_id=${this.clientId}&response_type=code&redirect_uri=${redirectUri}&approval_prompt=force&scope=read`;
+    }
+
+    async exchangeCodeForToken(code: string): Promise<any> {
+        const response = await axios.post('https://www.strava.com/oauth/token', {
+        client_id: this.clientId,
+        client_secret: this.clientSecret,
+        code,
+        grant_type: 'authorization_code',
+        });
+        return response.data;
+    }
+}
+
+export default new StravaService();
